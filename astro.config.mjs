d030164679f66cecc7cc7a29aacc765f1308d8b2
Tyler Mercer <acct@tylermercer.net>
2024-12-28 12:54:52 -0700

    Add astro-meta-tags dev toolbar app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
+import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [metaTags()],
 });
